c1d03437d12dd2211c965e5defa88a55a040c568
cscott brown <scott.brown@noaa.gov>
2024-11-26 04:29:23 -1000

    hai

--- local_lfs_to_gcs.py
+++ local_lfs_to_gcs.py
@@ -8,80 +8,17 @@ import requests
 import time
 import asyncio
 
-
+def get_temporary_path(self, oid):
+    """Compute the temporary path for a file based on its OID."""
+    base_dir = ".git/lfs/objects"
+    subdir = os.path.join(oid[:2], oid[2:4])
+    return os.path.join(base_dir, subdir, oid)
 
 class GCSLfsTransferAgent:
     def __init__(self):
         self.public_url = "https://storage.googleapis.com/{bucket}/PIFSC%2FSOD%2Fgit_lfs_test%2F{object_name}"
         self.private_url = "https://storage.googleapis.com/storage/v1/b/{bucket}/o/PIFSC%2FSOD%2Fgit_lfs_test%2F{object_name}?alt=media"
-
-    def get_access_token(self):
-        """Fetches the access token from gcloud."""
-        result = subprocess.run(
-            ["gcloud", "auth", "print-access-token"],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
-        )
-        if result.returncode != 0:
-            raise RuntimeError(f"Error fetching access token: {result.stderr}")
-        return result.stdout.strip()
-
-    async def handle_init(self, payload):
-        """Handle the init event from Git LFS."""
-        sys.stderr.write(f"Handling init: {payload}\n")
-        sys.stderr.flush()
-        sys.stdout.write(json.dumps({"event": "init"}) + "\n")
-        sys.stdout.flush()
-        sys.stderr.write("Init event processed and response flushed\n")
-
-    async def handle_terminate(self, payload):
-        """Handle the terminate event."""
-        sys.stderr.write("Handling terminate event\n")
-        sys.stdout.write(json.dumps({"event": "terminate"}) + "\n")
-        sys.stdout.flush()
-        sys.stderr.write("Terminate event processed\n")
-        sys.stderr.flush()
-        #sys.exit(0)
-    
-    def upload(self, bucket, object_name, file_path):
-        """Uploads a file to GCS using the REST API."""
-        upload_url = f"https://storage.googleapis.com/upload/storage/v1/b/{bucket}/o?uploadType=media&name={object_name}"
-        sys.stderr.write("uploading file")
-        sys.stderr.flush()
-
-        headers = {
-            "Content-Type": "application/octet-stream",
-            "Authorization": "Bearer {}".format(self.get_access_token())
-        }
-        with open(file_path, "rb") as file_data:
-            response = requests.post(upload_url, headers=headers, data=file_data)
-        
-        sys.stderr.write(f"response received: {response}")
-        sys.stderr.flush()
-
-        if response.status_code == 200:
-            sys.stderr.write(f"completing upload event")
-            sys.stderr.flush()
-            sys.stderr.write(f"{response.json}")
-            sys.stderr.flush()
-            sys.exit(0)
-            sys.stdout.write(
-              json.dumps({"event": "complete", "oid": object_name, "size": os.path.getsize(file_path)}) + "\n"
-            )
-            sys.stdout.flush()
-        else:
-            raise RuntimeError(f"Error uploading file: {response.status_code} {response.text}")
-        sys.stderr.write(f"upload event concluded")
-        sys.stderr.flush()
-    
-    async def handle_upload(self, payload):
-        """Handle the upload event."""
-        oid = payload.get("oid")
-        path = payload.get("path")
-        bucket = os.getenv("GCS_BUCKET")
-        sys.stderr.write(f"Handling upload for OID: {oid}, Path: {path}\n")
-        self.upload(bucket, oid, path)
+        self.upload_url = "https://storage.googleapis.com/upload/storage/v1/b/{bucket}/o?uploadType=media&name=PIFSC%2FSOD%2Fgit_lfs_test%2F{object_name}"
 
     def download(self, bucket, object_name, destination_path):
         """Downloads a file from GCS, handling both public and private buckets."""
@@ -91,8 +28,9 @@ class GCSLfsTransferAgent:
         # Try downloading as a public object
         response = requests.get(public_url, stream=True)
         
+        temp_path = get_temporary_path(object_name)
         if response.status_code == 200:
-            self._write_to_file(response, destination_path)
+            self._write_to_file(response, temp_path)
             sys.stdout.write(
                 json.dumps({"event": "complete", "oid": object_name, "path": destination_path}) + "\n"
             )
@@ -130,35 +68,74 @@ class GCSLfsTransferAgent:
             for chunk in response.iter_content(chunk_size=8192):
                 if chunk:  # Filter out keep-alive new chunks
                     file_data.write(chunk)
-    '''    
-    def handle_request(self):
-        """Main entry point for the transfer agent."""
-        sys.stderr.write("Handling request\n")
-        sys.stderr.flush()
-        # Read input from stdin
-        line = sys.stdin.readline()
-        payload = json.loads(line)
-        sys.stderr.write(f"Read payload: {payload}\n")
-        sys.stderr.flush()
-        action = payload.get("event")
-        if not bucket:
-            raise RuntimeError("Environment variable GCS_BUCKET is not set.")
+    def get_access_token(self):
+        """Fetches the access token from gcloud."""
+        result = subprocess.run(
+            ["gcloud", "auth", "print-access-token"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+        if result.returncode != 0:
+            raise RuntimeError(f"Error fetching access token: {result.stderr}")
+        return result.stdout.strip()
 
-        if action == "init":
-            sys.stderr.write("Detected init event\n")
+    async def handle_init(self, payload):
+        """Handle the init event from Git LFS."""
+        sys.stderr.write(f"Handling init: {payload}\n")
+        sys.stderr.flush()
+        sys.stdout.write(json.dumps({"event": "init"}) + "\n")
+        sys.stdout.flush()
+        sys.stderr.write("Init event processed and response flushed\n")
+
+    async def handle_terminate(self, payload):
+        """Handle the terminate event."""
+        sys.stderr.write("Handling terminate event\n")
+        sys.stdout.write(json.dumps({"event": "terminate"}) + "\n")
+        sys.stdout.flush()
+        sys.stderr.write("Terminate event processed\n")
+        sys.stderr.flush()
+        #sys.exit(0)
+    
+    def upload(self, bucket, object_name, file_path):
+        """Uploads a file to GCS using the REST API."""
+        sys.stderr.write("uploading file")
+        sys.stderr.flush()
+
+        upload_url = self.upload_url.format(bucket=bucket, object_name=object_name)
+
+        headers = {
+            "Content-Type": "application/octet-stream",
+            "Authorization": "Bearer {}".format(self.get_access_token())
+        }
+        with open(file_path, "rb") as file_data:
+            response = requests.post(upload_url, headers=headers, data=file_data)
+        
+        sys.stderr.write(f"response received: {response}")
+        sys.stderr.flush()
+
+        if response.status_code == 200:
+            sys.stderr.write(f"completing upload event")
             sys.stderr.flush()
-            self.handle_init(payload)
-        elif action == "terminate":
-            self.handle_terminate(payload)
-        elif action == "upload":
-            object_name = oid  # Use the object ID as the file name in GCS
-            self.upload(bucket, object_name, path)
-        elif action == "download":
-            object_name = oid
-            self.download(bucket, object_name, path)
+            sys.stderr.write(f"{response.json()}")
+            sys.stderr.flush()
+            sys.stdout.write(
+              json.dumps({"event": "complete", "oid": object_name, "size": os.path.getsize(file_path)}) + "\n"
+            )
+            sys.stdout.flush()
         else:
-            raise ValueError(f"Unsupported action: {action}")
-    '''
+            raise RuntimeError(f"Error uploading file: {response.status_code} {response.text}")
+        sys.stderr.write(f"upload event concluded")
+        sys.stderr.flush()
+    
+    async def handle_upload(self, payload):
+        """Handle the upload event."""
+        oid = payload.get("oid")
+        path = payload.get("path")
+        bucket = os.getenv("GCS_BUCKET")
+        sys.stderr.write(f"Handling upload for OID: {oid}, Path: {path}\n")
+        self.upload(bucket, oid, path)
+
 
     async def handle_request(self):
         """Main entry point for the transfer agent."""
